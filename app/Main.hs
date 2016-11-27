c4ab74732d854fc3b669d5436e40705435b95a49
Joshua McCartney <josh@mcbk.lan>
2016-11-26 19:37:15 -0500

    Changed to use the machine-utills package and added support for interfacelift.com

--- app/Main.hs
+++ app/Main.hs
@@ -15,57 +15,72 @@ import System.Process
 import MachineUtils
 import qualified Bing
 import qualified NatGeo
+import qualified IfaceLift
+import Network.HTTP.Simple
 
 data BackgroundSource
   = Bing
   | NatGeo
+  | Iface
   | Wait
   deriving (Read, Show)
 
+osaScript :: (Textual t, Semigroup t) => t -> t
 osaScript fileName = unlines [ "tell application \"System Events\""
                              , "\tset picture of every desktop to (\"" <> fileName <> "\" as POSIX file as alias)"
                              , "end tell"
                              ]
 
-restartDock = runCommand $ "killall Dock"
+restartDock :: MonadIO m => ProcessA (Kleisli m) (Event a) (Event ())
+restartDock = onEnd
+  >>> machine (const $ liftIO $ putStrLn "restartDock onEnd")
+  >>> machine (const $ liftIO restartDock_)
+  >>> machine (liftIO . waitForProcess)
+  >>> machine print
 
-dispatch :: MonadIO m => ProcessA (Kleisli m) (Event BackgroundSource) (Event String)
-dispatch = proc a -> do
-              cmdEvt <- id -< a
-              fin <- stopped -< a
-              cmd <- hold Wait -< cmdEvt
-              x <- gather -< [cmdEvt, fin]
+restartDock_ :: IO ProcessHandle
+restartDock_ = runCommand $ "killall Dock"
 
-              resEvt <- case cmd of
-                Bing -> edge >>> Bing.getImageUrl -< ()
-                NatGeo -> edge >>> NatGeo.getImageUrl -< ()
-                Wait -> edge >>> evMap (const "Waiting") -< ()
-              res <- hold "Nothing downloaded" -< resEvt
-              returnA -< res <$ x
-  
+dispatch :: MonadResource m => ProcessA (Kleisli m) (Event BackgroundSource) (Event String)
+dispatch = proc input -> do
+  cmdEvt <- hold Wait -< input
+
+  case cmdEvt of
+    Bing -> Bing.getImageUrl -< () <$ input
+    NatGeo -> NatGeo.getImageUrl -< () <$ input
+    Iface -> IfaceLift.getImageUrl -< () <$ input
+    Wait -> returnA -< noEvent
+
 instructions :: MonadIO m => m ()
 instructions = do
   putStrLn "Bing: download from Bing's picture of the day."
   putStrLn "NatGeo: download from National Geographic's picture of the day."
+  putStrLn "Iface: download from InterfaceLift."
   putStrLn "end: exits"
 
+parseUrlThrow' :: MonadThrow m => Manager -> String -> m (Request, Manager)
 parseUrlThrow' mgr url = do
-  req <- parseUrlThrow url
+  req <- setRequestHeaders ifaceRequestHeaders <$> parseUrlThrow url
   return (req, mgr)
 
 main :: IO ()
 main = do
   mgr <- newManager tlsManagerSettings
-  runResourceT $ runKleisli (run_ $ machine (const instructions)
-                             >>> inputCommand
-                             >>> dispatch
-                             >>> machine (parseUrlThrow' mgr)
-                             >>> makeRequest
-                             >>> sourceHttp_
-                             >>> downloadHttp "/tmp/bg.jpg"
-                             >>> tee
-                             >>> machine (const $ lift restartDock)
-                            ) (repeat ())
+  runRMachine_ (machine (const instructions)
+           >>> inputCommand'
+           >>> dispatch
+           >>> machine (parseUrlThrow' mgr)
+           >>> makeRequest
+           >>> sourceHttp_
+           >>> downloadHttp "/tmp/bg.jpg"
+           >>> tee
+           >>> restartDock
+               ) [()]
+
+ifaceRequestHeaders = [ ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36")
+                      , ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                      ]
+
   -- print urls
   -- reqs <- mapM parseUrlThrow urls
   -- runResourceT $ runKleisli (run_ (downloadHttp "/tmp/bg.jpg" >>> tee)) $ zip reqs (repeat mgr)
---
+++ src/IfaceLift.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE Arrows #-}
+
+module IfaceLift
+  ( getImageUrl ) where
+
+import MachineUtils
+import ClassyPrelude
+
+import Text.XML.HXT.Core
+import Text.XML.HXT.HTTP
+-- import Control.Arrow.Machine
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Simple
+
+import Control.Monad.Trans.Resource
+
+downloadBase :: String
+downloadBase = "https://interfacelift.com"
+
+randomWallpaperUrl :: String
+randomWallpaperUrl = "https://interfacelift.com/wallpaper/downloads/random/wide_16:10/2560x1600/"
+
+downloadWallpaperList :: (MonadResource m) => ProcessA (Kleisli m) (Event (Request, Manager)) (Event String)
+downloadWallpaperList = makeRequest >>> sourceHttp_ >>> evMap (unpack . decodeUtf8 . snd)
+
+machFold :: ArrowApply cat => ProcessA cat (Event String) (Event String)
+machFold = proc input -> do
+  res <- accum mempty -< (<>) <$> input
+  ed <- onEnd -< input
+  returnA -< res <$ ed
+
+parsePage :: MonadIO m => ProcessA (Kleisli m) (Event String) (Event String)
+parsePage = constructT kleisli0 $ do
+  str <- await
+  wallpaperUrls <- lift . liftIO $ runX $ readString [withValidate no, withParseHTML yes, withWarnings no, withHTTP mempty] str
+    //> hasAttrValue "id" (isPrefixOf "download")
+    /> hasName "a"
+    >>> getAttrValue "href"
+  mapM_ (\wpUrl -> yield $ downloadBase <> wpUrl) $ take 1 $ wallpaperUrls
+
+initialize :: (MonadThrow m, MonadIO m) => ProcessA (Kleisli m) (Event ()) (Event (Request, Manager))
+initialize = constructT kleisli0 $ do
+  _ <- await
+  req <- lift $ setRequestHeaders ifaceRequestHeaders <$> parseUrlThrow randomWallpaperUrl
+  mgr <- lift . liftIO $ newManager tlsManagerSettings
+  yield (req, mgr)
+
+getImageUrl :: MonadResource m => ProcessA (Kleisli m) (Event ()) (Event String)
+getImageUrl = initialize >>> downloadWallpaperList >>> machFold >>> parsePage
+
+ifaceRequestHeaders = [ ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36")
+                      , ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                      ]
